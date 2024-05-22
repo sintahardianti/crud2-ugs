29d8551b95d59d5bdcf5f096ca4b36bb28df97f9
Sinta <115516473+sintahardianti@users.noreply.github.com>
2024-05-22 14:58:14 +0700

    Create print.php

---
+++ print.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cetak Pengeluaran Barang</title>
+</head>
+
+<style>
+.header1 {
+    font-family: 'Trebuchet MS';
+    margin: auto;
+}
+
+h3 {
+    text-decoration: underline;
+    font-family: 'Trebuchet MS';
+    padding-left: 270px;
+}
+
+table,
+th,
+td {
+    border: 1px solid black;
+    padding-left: 4px;
+    border-collapse: collapse;
+    text-align: center;
+    height: 22px;
+    width: 905px;
+    font-family: 'Trebuchet MS';
+}
+
+.ttd {
+    padding: 10px;
+    height: 75px;
+}
+
+h4 {
+    font-size: 10px;
+    font-family: 'Trebuchet MS';
+    margin-top: 5px;
+}
+
+.t2 {
+    margin-top: 30px;
+    text-align: center;
+}
+
+tr.tr1 {
+    text-align: left;
+}
+</style>
+
+<header class="header1">
+    <p style="margin-bottom: 0; margin-top: 0;">PT. Unggul Semesta
+    <h3 style="margin-bottom: 0; margin-top: 0; margin-left: 50px;">BUKTI PENGELUARAN BARANG</h3>
+    </p>
+</header>
+
+<body>
+    <div class="container">
+        <table>
+            <th colspan="2" style="text-align:left;">No Barang: </th>
+            <th colspan="2" style="text-align:left;">User: </th>
+            <tr>
+            </tr>
+            <tr>
+                <th style="width:4%">No.</th>
+                <th style="width:47%">Nama Barang</th>
+                <th style="width:20%">Jumlah</th>
+                <th>Keterangan</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+        <br>
+        <table border="1" class="t2">
+            <tr>
+                <td colspan="2" class="t2"><span>Yang Mengeluarkan</span></td>
+                <td colspan="2" class="t2"><span>Mengetahui</span></td>
+                <td colspan="1" class="t2"><span>Yang Menerima</span></td>
+            </tr>
+            <tr>
+                <td>Karyawan ybs</td>
+                <td>Kepala Divisi</td>
+                <td>HRD & GA</td>
+                <td>Security</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="ttd"></td>
+                <td class="ttd"></td>
+                <td class="ttd"></td>
+                <td class="ttd"></td>
+                <td class="ttd"></td>
+            </tr>
+            <tr>
+                <td colspan="1" class="nama" style="text-align:center;">Sinta</td>
+                <td colspan="1" class="nama" style="text-align:center;"></td>
+                <td colspan="1" class="nama" style="text-align:center;"></td>
+                <td colspan="1" class="nama" style="text-align:center;"></td>
+            </tr>
+        </table>
+        </table>
+        <h4>DIVISI IT/UGS</h4>
+    </div>
+</body>
+
+</html>
